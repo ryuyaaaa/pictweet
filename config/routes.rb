035fe0f9e8ced6db0537a15cc675ccc2e3edbb62
Ryuya Nishino <ryuyasamaaaa@gmail.com>
2018-12-10 13:03:08 +0900

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'tweets#index'
-  get 'tweets' => 'tweets#index' # ツイート一覧画面
-  get 'tweets/new' => 'tweets#new' # ツイート投稿画面
-  post 'tweets' => 'tweets#create' # ツイート投稿機能
-  delete 'tweets/:id' => 'tweets#destroy'
-  patch 'tweets/:id' => 'tweets#update'
-  get 'users/:id' => 'users#show' # Mypageへのルーティング
-  get 'tweets/:id/edit' => 'tweets#edit'
-  get 'tweets/:id' => 'tweets#show' # ツイート詳細画面
+  root 'tweets#index'
+  resources :tweets                     #tweets_controllerに対してのresourcesメソッド
+  resources :users, only: [:show]       #users_controllerに対してのresourcesメソッド
 end
