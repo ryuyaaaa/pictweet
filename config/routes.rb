1de6dcb23735b808d5a73c0429b22d3b3532b134
Ryuya Nishino <ryuyasamaaaa@gmail.com>
2018-12-14 15:40:53 +0900

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'tweets#index'
-  root 'tweets#index'
-  resources :tweets                     #tweets_controllerに対してのresourcesメソッド
+  resources :tweets do
+    resources :comments, only: [:create]
+  end                  #tweets_controllerに対してのresourcesメソッド
   resources :users, only: [:show]       #users_controllerに対してのresourcesメソッド
 end
